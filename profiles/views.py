8361559c4d74ee0de2451fef1a6b8e5e9690c7d2
jadson <jadsonaluno@hotmail.com>
2018-08-11 09:20:50 -0300

    add index and cadastro views

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .forms import UserForm
 
 # Create your views here.
+def index(request):
+	return render(request,"index.html")
+
+def cadastro(request):
+    if(request.method=="POST"):
+        pass
+    else:
+        pass
+    return render(request,"cadastro_usuario.html",{"form":UserForm()})
