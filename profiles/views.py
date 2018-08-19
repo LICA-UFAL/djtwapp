a1cb81075bfde04dc5c15c9b5a1a759fa9130223
Francisco <fvcneto.master98@gmail.com>
2018-08-19 13:26:10 -0300

    create layout and new directory in templates

--- profiles/views.py
+++ profiles/views.py
@@ -3,4 +3,4 @@ from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    return render(request, "profiles/index.html")
